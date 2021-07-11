61c90ad6095c5d9dd0c81a88e5616b693e0ebd30
LuannMateus <luanmateuslima34@gmail.com>
2021-07-11 20:45:21 -0300

    Day four - Finish store back end

--- store-api/src/main.ts
+++ store-api/src/main.ts
@@ -2,16 +2,20 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './exception-filters/entity-not-found.exection-filter';
+import { RpcExceptionFilter } from './exception-filters/rpc.exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.useGlobalFilters(
+    new EntityNotFoundExceptionFilter(),
+    new RpcExceptionFilter(),
+  );
 
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
